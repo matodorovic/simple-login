9dcef7646c8db9073beea5a6164ae27fca9dc4b0
Marta Anna Todorovic <martaannatodorovic@gmail.com>
2023-04-12 18:45:39 +0200

    Changed regex for email

--- src/JS/validateEmail.js
+++ src/JS/validateEmail.js
@@ -1,12 +1,11 @@
 export default function validateEmail(mail) {
     let validEmail = document.querySelector("#validate-email");
     validEmail.innerHTML = "";
-  
-    let regEx = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
   
     if (mail === "") {
       validEmail.innerHTML = "Fyll i fältet";
-    } else if (regEx.test(mail)) {
+    } else if (mail.match(/(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi)) {
       return true;
     } else {
       validEmail.innerHTML = "Ogiltig email. Godkänd format: namn@exempel.se";
